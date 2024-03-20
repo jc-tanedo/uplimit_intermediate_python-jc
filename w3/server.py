cd089b6a9ea59e7da0ecff3a6c264375c689bd01
JC Tanedo <jcaezarrr@gmail.com>
2024-03-20 19:35:40 +0800

    w3 - implement server functions

--- w3/server.py
+++ w3/server.py
@@ -42,7 +42,7 @@ async def get() -> Dict:
     """
 
     ######################################## YOUR CODE HERE ##################################################
-
+    return dict({"status": "ok"})
     ######################################## YOUR CODE HERE ##################################################
 
 
@@ -53,7 +53,7 @@ async def get() -> HTMLResponse:
     should render the HTML file - index.html when a user goes to http://127.0.0.1:8000/
     """
     ######################################## YOUR CODE HERE ##################################################
-
+    return HTMLResponse(content=open("index.html", "r").read(), status_code=200)
     ######################################## YOUR CODE HERE ##################################################
 
 
@@ -64,5 +64,6 @@ async def get() -> List[ProcessStatus]:
     Get all the records from the process table and return it using the pydantic model ProcessStatus
     """
     ######################################## YOUR CODE HERE ##################################################
-
+    db = DB()
+    return db.read_all()
     ######################################## YOUR CODE HERE ##################################################
